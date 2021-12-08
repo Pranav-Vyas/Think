7bc6c9c09ee962fdb3f5cf70832881eb08147829
Suhani Dubey <suhanidubey98@gmail.com>
2021-12-08 18:38:58 +0530

    color change

--- frontend/src/components/HomeTitle/HomeTitle.jsx
+++ frontend/src/components/HomeTitle/HomeTitle.jsx
@@ -7,20 +7,20 @@ function HomeTitle() {
             {/* Nav Bar */}
             <div className="container-fluid">
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                    <a className="navbar-brand" href="">Brand-Name</a>
+                    <a className="navbar-brand" href="/">THINK</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="#footer">Contact</a>
+                                <a className="nav-link" href="/">About</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#pricing">Pricing</a>
+                                <a className="nav-link" href="#testimonials">Product</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#cta">Download</a>
+                                <a className="nav-link" href="#footer">Contact Us</a>
                             </li>
                         </ul>
                     </div>
